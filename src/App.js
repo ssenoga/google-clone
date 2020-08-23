be0472360050156845ce016b68d699d9e32ad25f
ssenoga <41122179+ssenoga@users.noreply.github.com>
2020-08-23 21:32:51 +0300

    Tired for now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./styles.css";
+
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This is the search page</h1>
+          </Route>
+          <Route path="">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
